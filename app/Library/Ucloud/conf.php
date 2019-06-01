9e086307798158ea49e1ef095f4c7de7a4de0137
yangypgit <43193229+yangypgit@users.noreply.github.com>
2019-06-01 12:45:35 +0800

    去掉一些ucloud信息

--- app/Library/Ucloud/conf.php
+++ app/Library/Ucloud/conf.php
@@ -8,7 +8,7 @@ global $UCLOUD_PRIVATE_KEY;
 $SDK_VER = "1.0.8";
 
 //空间域名后缀,请查看控制台上空间域名再配置此处
-$UCLOUD_PROXY_SUFFIX = '.cn-bj.ufileos.com';
+$UCLOUD_PROXY_SUFFIX = '';
 
-$UCLOUD_PUBLIC_KEY = 'tiJ1j4UQWwnvJxqKNw4LV7eyUYbGgYmb/KRoWwNbQIrE/G+Ym32j+g==';
-$UCLOUD_PRIVATE_KEY = '445247083c99f940ec99347ea8714c563d5f297d';
+$UCLOUD_PUBLIC_KEY = '';
+$UCLOUD_PRIVATE_KEY = '';
